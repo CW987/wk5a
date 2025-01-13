4c260b769bdc76881b20faac5e2338b44a1da374
“hxc338” <hxc338@student.bham.ac.uk>
2025-01-13 16:19:08 +0000

    set up port for server to listen and root route

--- server/server.js
+++ server/server.js
@@ -4,8 +4,20 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const dbConnectionString = process.env.DATABASE_URL;
 
 export const db = new pg.Pool({
     connectionString: dbConnectionString,});
+
+app.listen(8080, () => {
+    console.log("The server is running on port 8080")
+});
+
+app.get("/", (req, res) =>{
+    res.json({message: "This is the root route"})
+});
+
