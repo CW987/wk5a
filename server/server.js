3ea36a07a3879402f38580488c7660cd325b6c16
Curtis Witter <inward-panting0f@icloud.com>
2025-01-13 13:46:02 +0000

    Express Serve & PG

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// import packages
+import pg from 'pg';
+import dotenv from 'dotenv';
+// start or configure packages
+dotenv.config();
+// tell server to use JSON
+// set up a port for the server by listening... 
+// setup database pool
+// create a root route
+// ========================
+
+// Route needed to READ data from database
+// Route needed to CREATE data in the database
