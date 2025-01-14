2075e09c7d04d2b7683d4f90ea20169607fe1ad3
eminehorozoglu <emineh86@gmail.com>
2025-01-14 13:49:07 +0000

    change date type on main js

--- client/src/main.js
+++ client/src/main.js
@@ -42,39 +42,49 @@ async function messages() {
       console.log(MessageBox);
       async function MessagesDetail() {
        for (let i = 0; i < MessageBox[0].length; i++) {
-          console.log(i);
+          //console.log(i);
     
           const MessageContainer = document.createElement("div");
           MessageContainer.className = "message-container";
           Messageshop.appendChild(MessageContainer);
     
           const date = document.createElement("h4");
-          date.textContent = `"${MessageBox[0][i].date}"`;
+         const date1 = new Date(`${MessageBox[0][i].date}`)
+         const dateStr = date1.toLocaleString('en-US', {
+            timeZone: 'America/New_York'
+          })
+          const date2 = new Date (dateStr)
+          var dd = date2.getDate();
+          var yyyy = date2.getFullYear();
+          var mm = date2.getMonth()+1
+          console.log(dd + "/"+mm + "/"+ yyyy)
+          const date3 = dd + "/"+mm+"/"+yyyy
+          date.textContent = date3;
           MessageContainer.appendChild(date);
     
           
           const MessageText = document.createElement("p");
-          MessageText.textContent = `"${MessageBox[0][i].my_note}"`;
+          MessageText.textContent = `${MessageBox[0][i].my_note}`;
           MessageText.className = "message-text";
           MessageContainer.appendChild(MessageText);
 
           const myexercise = document.createElement("p");
-          myexercise.textContent = `"${MessageBox[0][i].exercise}"`;
+          myexercise.textContent = `${MessageBox[0][i].exercise}`;
           myexercise.className = "exercise";
           MessageContainer.appendChild(myexercise);
 
           const mysets = document.createElement("p");
-          mysets.textContent = `"${MessageBox[0][i].sets}"`;
+          mysets.textContent = `${MessageBox[0][i].sets}`;
           mysets.className = "exercise";
           MessageContainer.appendChild(mysets);
 
           const myreps = document.createElement("p");
-          myreps.textContent = `"${MessageBox[0][i].reps}"`;
+          myreps.textContent = `${MessageBox[0][i].reps}`;
           myreps.className = "exercise";
           MessageContainer.appendChild(myreps);
 
           const myweight = document.createElement("p");
-          myweight.textContent = `"${MessageBox[0][i].weight}"`;
+          myweight.textContent = `${MessageBox[0][i].weight}`;
           myweight.className = "exercise";
           MessageContainer.appendChild(myweight);
     
