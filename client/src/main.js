97fa5bc56a5eea671f97c4f7592467c05a0b25e7
eminehorozoglu <emineh86@gmail.com>
2025-01-14 11:58:38 +0000

    create client js

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,90 @@
+const workoutdiary = document.querySelector("#input-form");
+
+
+function handleSubmitMessageForm(event) {
+  event.preventDefault();
+
+  const formData = new FormData(workoutdiary);
+  const formValues = Object.fromEntries(formData);
+
+
+  
+  fetch("http://localhost:8080/new-data", {
+   
+    method: "POST",
+   
+    headers: {
+      "Content-Type": "application/json",
+    },
+ 
+    body: JSON.stringify({formValues}),
+
+  });
+  console.log(formValues);
+  setTimeout(workoutdiary.onsubmit = function(){
+   alert("Message submitted!")
+    //location.reload(true);
+}
+, 3000)
+
+}
+workoutdiary.addEventListener("submit", handleSubmitMessageForm);
+
+const Messageshop = document.getElementById("message");
+let MessageBox = [];
+
+async function messages() {
+    const response = await fetch(
+        "http://localhost:8080/comments"
+      );
+      const data = await response.json();
+      MessageBox.push(data);
+      console.log(MessageBox);
+      async function MessagesDetail() {
+       for (let i = 0; i < MessageBox[0].length; i++) {
+          console.log(i);
+    
+          const MessageContainer = document.createElement("div");
+          MessageContainer.className = "message-container";
+          Messageshop.appendChild(MessageContainer);
+    
+          const date = document.createElement("h4");
+          date.textContent = `"${MessageBox[0][i].date}"`;
+          MessageContainer.appendChild(date);
+    
+          
+          const MessageText = document.createElement("p");
+          MessageText.textContent = `"${MessageBox[0][i].my_note}"`;
+          MessageText.className = "message-text";
+          MessageContainer.appendChild(MessageText);
+
+          const myexercise = document.createElement("p");
+          myexercise.textContent = `"${MessageBox[0][i].exercise}"`;
+          myexercise.className = "exercise";
+          MessageContainer.appendChild(myexercise);
+
+          const mysets = document.createElement("p");
+          mysets.textContent = `"${MessageBox[0][i].sets}"`;
+          mysets.className = "exercise";
+          MessageContainer.appendChild(mysets);
+
+          const myreps = document.createElement("p");
+          myreps.textContent = `"${MessageBox[0][i].reps}"`;
+          myreps.className = "exercise";
+          MessageContainer.appendChild(myreps);
+
+          const myweight = document.createElement("p");
+          myweight.textContent = `"${MessageBox[0][i].weight}"`;
+          myweight.className = "exercise";
+          MessageContainer.appendChild(myweight);
+    
+          
+    }
+      }
+      MessagesDetail();
+    }
+    
+    messages();
+    
+
 
--- server/server.js
+++ server/server.js
@@ -1,25 +1,33 @@
 import express from "express";
 import cors from "cors";
-import pg from "pg";
-import dotenv from "dotenv";
 
-dotenv.config();
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const dbConnectionString = process.env.DATABASE_URL;
-
-export const db = new pg.Pool({
-    connectionString: dbConnectionString,});
-
-app.listen(8080, () => {
+app.post("/messages", function (req, res) {
+    console.log("This is the req.body", req.body);
+    res.json({ status: "Message Received" });
+  
+  });
+  app.listen(8080, () => {
     console.log("The server is running on port 8080")
 });
 
 app.get("/", (req, res) =>{
     res.json({message: "This is the root route"})
 });
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const dbConnectionString = process.env.DATABASE_URL;
+
+export const db = new pg.Pool({
+    connectionString: dbConnectionString,});
 
 app.get("/comments", async (req, res) => {
     const query = await db.query(`SELECT * FROM workout_diary`);
