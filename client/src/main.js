228ae786379bcb14ff22911b575eef82e60035c7
eminehorozoglu <emineh86@gmail.com>
2025-01-15 15:23:36 +0000

    add list button fuction

--- client/src/main.js
+++ client/src/main.js
@@ -97,5 +97,108 @@ async function messages() {
     
     messages();
     
+    const filtremessagebutton = document.getElementById("list");
+    const filtrebox = document.getElementById("message1");
+    let filtre = [];
+    
+    async function filtremessages() {
+        const response = await fetch(
+            "http://localhost:8080/comments"
+          );
+          const data = await response.json();
+          filtre.push(data);
 
+        
+          async function MessagesDetailfiltre() {
+           for (let i = 0; i < filtre[0].length; i++) {
+           const id = `${filtre[0][i].id}`;
+           console.log(id);
+          
+           const date1 = new Date(`${filtre[0][i].date}`)
+           const dateStr = date1.toLocaleString('en-US', {
+              timeZone: 'America/New_York'
+            })
+            const date2 = new Date (dateStr)
+            var dd = date2.getDate();
+            var yyyy = date2.getFullYear();
+            var mm = date2.getMonth()+1
+           // console.log(dd + "/"+mm + "/"+ yyyy)
+            const date3 = yyyy + "-"+mm+"-"+dd
+
+           
+
+
+ 
+
+           var t;
+           t = document.getElementById("filterdate").value; 
+           const newT = new Date(t);          
+                var dd1 = newT.getDate();
+               var yyyy1 = newT.getFullYear();
+               var mm1 = newT.getMonth()+1
+               const filterT = yyyy1 + "-"+mm1+"-"+dd1
+         
+            
+
+           if (filterT === date3){
+         
+              const MessageContainer = document.createElement("div");
+              MessageContainer.className = "message-container";
+              filtrebox.appendChild(MessageContainer);
+
+              const date = document.createElement("h4");
+              const date1 = new Date(`${MessageBox[0][i].date}`)
+              const dateStr = date1.toLocaleString('en-US', {
+                 timeZone: 'America/New_York'
+               })
+               const date2 = new Date (dateStr)
+               var dd = date2.getDate();
+               var yyyy = date2.getFullYear();
+               var mm = date2.getMonth()+1
+              // console.log(dd + "/"+mm + "/"+ yyyy)
+               const date3 = dd + "/"+mm+"/"+yyyy
+               date.textContent = date3;
+               MessageContainer.appendChild(date);
+
+              const FiltreText = document.createElement("p");
+              FiltreText.textContent = `${filtre[0][i].my_note}`;
+              FiltreText.className = "message-text";
+              MessageContainer.appendChild(FiltreText);
+
+              const myexercise = document.createElement("p");
+              myexercise.textContent = `Exercise: ${MessageBox[0][i].exercise}`;
+              myexercise.className = "exercise";
+              MessageContainer.appendChild(myexercise);
+    
+              const mysets = document.createElement("p");
+              mysets.textContent = `Sets: ${MessageBox[0][i].sets}`;
+              mysets.className = "sets";
+              MessageContainer.appendChild(mysets);
+    
+              const myreps = document.createElement("p");
+              myreps.textContent = `Reps: ${MessageBox[0][i].reps}`;
+              myreps.className = "reps";
+              MessageContainer.appendChild(myreps);
+    
+              const myweight = document.createElement("p");
+              myweight.textContent = `Weight: ${MessageBox[0][i].weight}`;
+              myweight.className = "weight";
+              MessageContainer.appendChild(myweight);
+        
+            
+          }
+           
+        }
+        
+        
+    }
+        MessagesDetailfiltre(filtre);
+      }
+
+      function clear() {
+        filtrebox.innerHTML = null;
+      }
+
+      filtremessagebutton.addEventListener("click",filtremessages);
+      filtremessagebutton.addEventListener("click",clear);
 
